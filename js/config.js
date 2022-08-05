9396cf22c3585dffffdcee692782e496c7c673de
ticlacalvin <ticlacalvin@yahoo.com>
2022-08-04 23:44:04 -0400

    modified the config.js file

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_0qOlAoQG8", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1ddgh2l51ivj90bmcf07gus5t5", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
